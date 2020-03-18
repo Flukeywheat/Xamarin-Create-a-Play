f35e11c9fd706e2950e3b7183850a0b54469077a
flukeywheat <Chapter_120@yahoo.com>
2020-03-18 15:35:16 -0500

    Added new Main Page with basic navigation to skeleton pages

--- FootballPlaybookCreater/MainPage.xaml.cs
+++ FootballPlaybookCreater/MainPage.xaml.cs
@@ -8,14 +8,30 @@ using Xamarin.Forms;
 
 namespace FootballPlaybookCreater
 {
-    // Learn more about making custom code visible in the Xamarin.Forms previewer
-    // by visiting https://aka.ms/xamarinforms-previewer
-    [DesignTimeVisible(false)]
+    
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void NewPlaybook_Clicked(object sender, EventArgs e)
+        {           
+            Navigation.PushAsync(new NewPlaybookPage());
+        }
+
+        private void LoadPlaybook_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new LoadPlaybookPage());
+        }
+        private void Settings_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Settings());
+        }
+        private void Exit_Clicked(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
         }
     }
 }
