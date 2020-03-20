5d551cb84d8834549d7f69a0224c2088e91c4f46
flukeywheat <Chapter_120@yahoo.com>
2020-03-20 14:47:28 -0500

    Started Implementation of SQLite and Added a few event handlers for testing CRUD operations

--- FootballPlaybookCreater/NewPlaybookPage.xaml.cs
+++ FootballPlaybookCreater/NewPlaybookPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,9 @@ namespace FootballPlaybookCreater
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class NewPlaybookPage : ContentPage
     {
+        #region Properties and Constructors
         private SQLiteAsyncConnection _connection;
+        private ObservableCollection<BaseFormation> playbooks;
         public NewPlaybookPage()
         {
             InitializeComponent();
@@ -37,18 +40,41 @@ namespace FootballPlaybookCreater
             base.OnAppearing();
             
             await _connection.CreateTableAsync<BaseFormation>();
-            var baseFormations = await _connection.Table<BaseFormation>().ToListAsync();
-
-
+            playbooks = new ObservableCollection<BaseFormation>(await _connection.Table<BaseFormation>().ToListAsync());
+            GetBaseFormations();
         }
+        #endregion
+        #region Event Handlers
 
         private void Offense_Defense_Switch_OnChanged(object sender, ToggledEventArgs e)
         {
             Offense_Defense_Switch.Text = (e.Value) ? "Defense" : "Offense";
         }
+
+        private void AddButton_Clicked(object sender, EventArgs e)
+        {
+            BaseFormation newPlaybook = new BaseFormation {Name = playbookNameEntryCell.Text };
+            _connection.InsertAsync(newPlaybook);
+            playbooks.Add(newPlaybook);
+        }
+
+        #endregion
+
+        #region HelperFunctions
+        private void GetBaseFormations()
+        {
+            if ( playbooks != null)
+            {
+                foreach (var formation in playbooks)
+                {
+                    BaseFormationsPicker.ItemsSource.Add(formation);
+                }
+            }           
+        }
+        #endregion
     }
 
-    
 
-    
+
+
 }
